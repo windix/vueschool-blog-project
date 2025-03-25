2b3e913d05892bee69e4b0e95de30f604d58eb9f
Windix Feng <windix@gmail.com>
2025-03-25 13:38:09 +1100

    feat: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <script setup lang="ts"></script>
 
-<template>Hello</template>
+<template>
+  <div
+    class="h-[200px] w-[400px] bg-blue-500 m-3 text-[130px] text-amber-100 p-10 font-extralight tracking-widest rounded-2xl shadow-2xl"
+  >
+    Hello
+  </div>
+</template>
