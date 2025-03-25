373fd4e2d61c820dbfa18bc2b9b5b14f909edfe7
Windix Feng <windix@gmail.com>
2025-03-25 19:15:19 +1100

    fix: branch name

--- scripts/build-info.sh
+++ scripts/build-info.sh
@@ -2,5 +2,5 @@
 echo "Date: $(date)" > dist/build-info.txt
 echo "Node version: $(node --version)" >> dist/build-info.txt
 echo "Git commit: $(git rev-parse HEAD)" >> dist/build-info.txt
-echo "Git branch: $(git rev-parse --abbrev-ref HEAD)" >> dist/build-info.txt
+echo "Git branch: $(git branch | grep '*' | awk '{print $2}')" >> dist/build-info.txt
 echo "Server: $(uname -a)" >> dist/build-info.txt
